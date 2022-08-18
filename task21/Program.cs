934b6a41513aff9d6fa1af3ae365c6edf5e9b8f0
Oleg <olegrem1020@mail.ru>
2022-08-18 17:26:34 +0600

    add task 19-29

---
+++ task21/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты трёх
+// точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.5
+
+
+Console.WriteLine("Введите координату точки А по X");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки А по Y");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки A по Z");
+int za = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по X");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по Y");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату точки B по Z");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(xa-xb ,2) + Math.Pow(ya - yb ,2) + Math.Pow(za - zb ,2));
+
+Console.WriteLine($"{res:f2}");
+
---
+++ task27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int mod = 0;
+int sum = 0;
+while(num > 0)
+{
+
+    mod = num % 10;
+    num = num / 10;
+
+    sum = sum + mod;
+}
+
+Console.WriteLine($"Сумма цифр числа {sum}");
