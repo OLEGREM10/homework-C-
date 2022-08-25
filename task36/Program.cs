de7d71693e5dcf2819f8ce519e27593ca0860a58
Oleg <olegrem1020@mail.ru>
2022-08-25 18:09:15 +0600

    add task 34-38

---
+++ task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+int sumnumbers = 0;
+//int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 21);
+    while(i % 2 != 0)
+    {
+        sumnumbers += array[i];
+        i++;
+    }
+    
+
+}
+Console.Write(string.Join("," , array));
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных чисел = {sumnumbers}");
---
+++ task38/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между
+//  максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = new double [10];
+double max = array[0];
+double min = array[1];
+double differens = 0;
+
+for(int i = 0; i < array.Length; i++ )
+{
+    array[i] = new  Random().Next(50, 100);
+    
+    while(array[i] > max )
+    {
+        max = array[i];
+        i++;
+        
+    }
+    while(array[i] < min)
+    {
+        min = array[i];
+        i++;
+
+    }
+    
+    
+    
+}
+differens = (max - min);
+Console.WriteLine(string.Join("," , array));
+Console.WriteLine($"Разница между максимальным {max} и минамальным {min} = {differens}");
