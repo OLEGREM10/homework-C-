4e7a22ba13a97f8df9117a1a611c2daeadd57748
Oleg <olegrem1020@mail.ru>
2022-09-05 19:27:03 +0600

    Добавил задачи 64,66

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+using System;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Queue(int M, int N)
+            {
+                if (M > N) return " ";
+                else return $"{M}, " + Queue(M + 1,N);
+            }
+             Console.WriteLine(Queue(1,15));
+
+        }
+
+    }
+}
---
+++ task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using System;
+
+namespace numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum(int M, int N)
+            {
+                if (M > N) return 0;
+                else return M + Sum(M + 1,N);
+            }
+             Console.WriteLine(Sum(4,8));
+
+        }
+
+    }
+}
